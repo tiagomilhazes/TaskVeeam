5534726118f72a34dd0300cbd9b1e13c43dd7477
tiagomilhazes <tiagomilhazes@gmail.com>
2023-01-27 16:20:08 +0000

    Loops until find the process and terminates it after X seconds

---
+++ TaskVeeam/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+
+    int threshold = 10; // threshold in seconds
+    string processName = "Notepad";
+    while (true)
+    {
+    var process = Process.GetProcessesByName(processName).FirstOrDefault();
+    
+    if (process != null) {
+
+        try
+        {
+            if (process.StartTime < DateTime.Now.AddSeconds(-threshold))
+            {
+                Console.WriteLine($"Killing process {process.ProcessName} that has been running for {(DateTime.Now - process.StartTime).TotalSeconds} seconds.");
+                process.Kill();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        Thread.Sleep(1000);
+    }
+           
+    }
+
